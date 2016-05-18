2dd44c6ca9cd8ff9cb91ba571072a5417eaa81b8
Taro Murao <taro.murao@gmail.com>
2016-05-18 12:24:36 +0200

    ii13-15 solutions implemented.

--- src/ii_collections/_15_AllAnyAndOtherPredicates.kt
+++ src/ii_collections/_15_AllAnyAndOtherPredicates.kt
@@ -1,5 +1,7 @@
 package ii_collections
 
+import android.util.Log
+
 fun example2(list: List<Int>) {
 
     val isZero: (Int) -> Boolean = { it == 0 }
@@ -15,25 +17,25 @@ fun example2(list: List<Int>) {
 
 fun Customer.isFrom(city: City): Boolean {
     // Return true if the customer is from the given city
-    todoCollectionTask()
+    return this.city == city
 }
 
 fun Shop.checkAllCustomersAreFrom(city: City): Boolean {
     // Return true if all customers are from the given city
-    todoCollectionTask()
+    return this.customers.all { it.isFrom(city) }
 }
 
 fun Shop.hasCustomerFrom(city: City): Boolean {
     // Return true if there is at least one customer from the given city
-    todoCollectionTask()
+    return this.customers.any { it.isFrom(city) }
 }
 
 fun Shop.countCustomersFrom(city: City): Int {
     // Return the number of customers from the given city
-    todoCollectionTask()
+    return this.customers.count { it.isFrom(city) }
 }
 
 fun Shop.findAnyCustomerFrom(city: City): Customer? {
     // Return a customer who lives in the given city, or null if there is none
-    todoCollectionTask()
+    return this.customers.find { it.isFrom(city) }
 }
