4e3924f7ad80dcdcad0620d9021b8ee9d89d1638
Taro Murao <taro.murao@gmail.com>
2016-05-26 11:20:48 +0200

    solutions to 25-27 implemented. Gave up with 28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,46 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.text.SimpleDateFormat
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
 
-enum class TimeInterval {
-    DAY,
-    WEEK,
-    YEAR
+    override fun compareTo(other: MyDate): Int = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+
+    operator fun rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+    fun toDate() = SimpleDateFormat("yyyy-MM-dd").parse("$year-${"%02d".format(month)}-${"%02d".format(dayOfMonth)}")
+
+    fun nextDay(): MyDate {
+        val cal = Calendar.getInstance()
+        cal.time = this.toDate()
+        cal.add(Calendar.DATE, 1)
+        return MyDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
+    }
+
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+enum class TimeInterval { DAY, WEEK, YEAR }
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate):
+        ClosedRange<MyDate>, Iterable<MyDate> {
+
+    override fun iterator(): Iterator<MyDate> = MyDateIterator(this)
+
+    class MyDateIterator(val range: DateRange): Iterator<MyDate> {
+        private var current: MyDate = range.start
+
+        override fun next(): MyDate {
+            val result = current
+            current = current.nextDay()
+            return result
+        }
+
+        override fun hasNext(): Boolean = current <= range.endInclusive
+    }
+
+}
