c40b95146a46785c3637cd9869e4590c3191cad7
Taro Murao <taro.murao@gmail.com>
2016-06-08 14:57:11 +0200

    added solution to 31

--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,7 +3,18 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable {
+    var count = 0
+
+    operator fun invoke(): Invokable {
+        count += 1
+        return this
+    }
+
+    fun getNumberOfInvocations(): Int {
+        return count
+    }
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +25,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
